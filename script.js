58fcdeee4da827ce8cfed5fd166a3f9edf95048c
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-06 13:28:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,32 @@
   outputDiv.textContent =  selectedDate;
         }
 
+ const searchInput = document.getElementById('searchInput');
+  const searchResults = document.getElementById('searchResults');
 
+  // Array of month names
+  const months = [
+    'January', 'February', 'March', 'April',
+    'May', 'June', 'July', 'August',
+    'September', 'October', 'November', 'December'
+  ];
+
+  // Add an event listener to the input field to capture user input
+  searchInput.addEventListener('input', function () {
+    const query = searchInput.value.toLowerCase();
+
+    // Clear previous search results
+    searchResults.innerHTML = '';
+
+    // Filter months that match the user's input
+    const matchingMonths = months.filter(month => month.toLowerCase().includes(query));
+
+    // Display matching months
+    matchingMonths.forEach(month => {
+      const resultItem = document.createElement('div');
+      resultItem.textContent = month;
+      searchResults.appendChild(resultItem);
+    });
+  });
 
     
