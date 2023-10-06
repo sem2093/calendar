703f10e9d3c090a73f404022e1239f186f492be2
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-06 13:00:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,33 +8,9 @@
             // Display the selected date (you can perform any further processing here)
             alert("Selected Date: " + selectedDate);
 
-  outputDiv.innerHTML = '<p>' + selectedDate + '</p>';
+  outputDiv.textContent =  selectedDate;
         }
 
-function timeSlot() {
-            // Get the input element by its ID
-            const dateInput = document.getElementById('dateInput');
 
-            // Get the selected date value
-            const selectedDate = dateInput.value;
-
-            // Display the selected date (you can perform any further processing here)
-            alert("Selected Date: " + selectedDate);
-
-  timeSlot.innerHTML = '<p>' + selectedDate + '</p>';
-        }
-
-function timePlan() {
-            // Get the input element by its ID
-            const dateInput = document.getElementById('dateInput');
-
-            // Get the selected date value
-            const selectedDate = dateInput.value;
-
-            // Display the selected date (you can perform any further processing here)
-            alert("Selected Date: " + selectedDate);
-
-  timePlan.textContent = selectedDate;
-        }
 
     
