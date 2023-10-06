60501236114947b6f6aa21e1b0d7acbf1c7441da
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-06 12:51:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,31 @@
 
   outputDiv.innerHTML = '<p>' + selectedDate + '</p>';
         }
+
+function timeSlot() {
+            // Get the input element by its ID
+            const dateInput = document.getElementById('dateInput');
+
+            // Get the selected date value
+            const selectedDate = dateInput.value;
+
+            // Display the selected date (you can perform any further processing here)
+            alert("Selected Date: " + selectedDate);
+
+  timeSlot.innerHTML = '<p>' + selectedDate + '</p>';
+        }
+
+function timePlan() {
+            // Get the input element by its ID
+            const dateInput = document.getElementById('dateInput');
+
+            // Get the selected date value
+            const selectedDate = dateInput.value;
+
+            // Display the selected date (you can perform any further processing here)
+            alert("Selected Date: " + selectedDate);
+
+  timePlan.textContent = selectedDate;
+        }
+
     
