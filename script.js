5392b75f0dfd73ca9b7d93145c49998c9f5f22e9
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-06 11:34:39 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+        function captureDate() {
+            // Get the input element by its ID
+            const dateInput = document.getElementById('dateInput');
+
+            // Get the selected date value
+            const selectedDate = dateInput.value;
+
+            // Display the selected date (you can perform any further processing here)
+            alert("Selected Date: " + selectedDate);
+        }
+    </script>
