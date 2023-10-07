09a1099c9c73cc248964654c6a2d53e5d3441cfb
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-07 12:10:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -19,5 +19,16 @@
             taskList.removeChild(li);
         }
         
-    
+    function darkMode(){
+// Get a reference to the <html> tag
+var htmlTag = document.documentElement;
+// Add an attribute and value to the <html> tag
+htmlTag.setAttribute("data-bs-theme", "dark");
+}
+function lightMode(){
+// Get a reference to the <html> tag
+var htmlTag = document.documentElement;
+// Add an attribute and value to the <html> tag
+htmlTag.setAttribute("data-bs-theme", "light");
+}
 
