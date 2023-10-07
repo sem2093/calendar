d80b2e114a25d7a07b7b54270921f309f68afbb8
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-07 04:45:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
+        const taskInput = document.getElementById('taskInput');
+        const taskList = document.getElementById('taskList');
+
+        function addTask() {
+            const taskText = taskInput.value.trim();
+            if (taskText === '') return; // Ignore empty tasks
+
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <span>${taskText}</span>
+                <button onclick="removeTask(this)">Delete</button>
+            `;
+            taskList.appendChild(li);
+            taskInput.value = '';
+        }
+
+        function removeTask(button) {
+            const li = button.parentElement;
+            taskList.removeChild(li);
+        }
+    
 
