0040317f4649a0fa992420755c2383a4690cfd8f
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-20 13:17:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,31 @@
 const clock = document.getElementById("now");
 const currentTime = new Date();
 clock.innerHTML= currentTime;
-const formBox = document.getElementById("formBox");
+const title= document.getElementById("appointmentTitle").value;
 // create task form generation
  function createTask() {
   // define title for task/Event 
- const title= document.getElementById("appointmentTitle").value;
-            const taskForm= document.createElement('form');
-            taskForm.innerHTML = `
+            const parentElement = document.createElement("div");
+  const newChildElement = document.createElement("form");
+            newChildElement.innerHTML = `
             <h5 class="appointmentTitle">${title}</h5>
              <label><input type="text" class="taskNotes">Add Note</label>
              <label><input type="date" class="deadLine"> Add Deadline</label>
                 <button onclick="addTask(this)" class="btn btn-success">Add To Calendar</button>
                 <button onclick="remove(this)" class="btn btn-danger">Cancel</button>
             `;
-           formBox.appendChild(form);
+           parentElement.appendChild(newChildElement);
+
+  const existingParent = document.getElementById("formBox");
+  existingParent.appendChild(parentElement);
             }
 //create event form generation
 function createEvent() {
  // define title for task/Event 
-const title= document.getElementById("appointmentTitle").value;
-            const eventForm= document.createElement('form');
-            eventForm.innerHTML = `
+
+            const parentElement2 = document.createElement("div");
+  const newChildElement2 = document.createElement("form");
+            newChildElement2.innerHTML = `
             <h5 class="appointmentTitle">${title}</h5>
              <label><input type="text" class="eventNotes">Add Note</label>
              <label>Begins<input type="date" class="eventStart"></label>
@@ -30,15 +34,18 @@ const title= document.getElementById("appointmentTitle").value;
                 <button onclick="addEvent(this)" class="btn btn-success">Add To Calendar</button>
                 <button onclick="remove(this)" class="btn btn-danger">Cancel</button>
             `;
-           formBox.appendChild(form);
+           parentElement2.appendChild(newChildElement2);
+ const existingParent2 = document.getElementById("formBox");
+  existingParent2.appendChild(parentElement2);
             }
+ 
+            
   function remove(button) {
-            const form = button.parentElement;
-            formBox.removeChild(form);
+            const form = button.newChildElement;
+            parentElement.removeChild(form);
         }
 //process task form 
 function addTask(){
- const title= document.querySelector(".appointmentTitle").innerText;
  const taskNotes = document.querySelector(".taskNotes").value;
  const deadLine = document.querySelector(".deadLine").value;
  const form = document.querySelector("form");
@@ -47,20 +54,19 @@ function addTask(){
 }
 //process event form 
 function addEvent(){
- const title= document.querySelector(".appointmentTitle").innerText;
  const taskNotes = document.querySelector(".taskNotes").value;
 const form = document.querySelector("form");
  formBox.removeChild(form);
  
 }
-
+const colorMode = document.getElementById("colorMode");
 const isDark= false;
-function colorMode(){ 
+colorMode.addEventListener= ("change", function(event){ 
 var htmlTag = document.documentElement;
 if (isDark) { htmlTag.setAttribute("data-bs-theme", "light");
              isDark= false; } 
 else { htmlTag.setAttribute("data-bs-theme", "dark");
         isDark = true; }
-}
+});
 
 
