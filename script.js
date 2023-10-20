f0634fd76e0af3f0daeb38f7bc3af99840f2c025
Steven Miller <131418264+sem2093@users.noreply.github.com>
2023-10-20 13:45:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const title= document.getElementById("appointmentTitle").value;
             const parentElement = document.createElement("div");
   const newChildElement = document.createElement("form");
             newChildElement.innerHTML = `
-            <h5 class="appointmentTitle">${title}</h5>
+            <h5>${title}</h5>
              <label><input type="text" class="taskNotes">Add Note</label>
              <label><input type="date" class="deadLine"> Add Deadline</label>
                 <button onclick="addTask(this)" class="btn btn-success">Add To Calendar</button>
@@ -24,19 +24,19 @@ const title= document.getElementById("appointmentTitle").value;
 function createEvent() {
  // define title for task/Event 
 
-            const parentElement2 = document.createElement("div");
-  const newChildElement2 = document.createElement("form");
-            newChildElement2.innerHTML = `
-            <h5 class="appointmentTitle">${title}</h5>
+            const parentElement = document.createElement("div");
+  const newChildElement = document.createElement("form");
+            newChildElement.innerHTML = `
+            <h5>${title}</h5>
              <label><input type="text" class="eventNotes">Add Note</label>
              <label>Begins<input type="date" class="eventStart"></label>
              <label>Ends<input type="date" class="eventEnd"></label>
                 <button onclick="addEvent(this)" class="btn btn-success">Add To Calendar</button>
                 <button onclick="remove(this)" class="btn btn-danger">Cancel</button>
             `;
-           parentElement2.appendChild(newChildElement2);
- const existingParent2 = document.getElementById("formBox");
-  existingParent2.appendChild(parentElement2);
+           parentElement.appendChild(newChildElement);
+ const existingParent = document.getElementById("formBox");
+  existingParent.appendChild(parentElement);
             }
  
             
@@ -66,7 +66,7 @@ var htmlTag = document.documentElement;
 if (isDark) { htmlTag.setAttribute("data-bs-theme", "light");
              isDark= false; } 
 else { htmlTag.setAttribute("data-bs-theme", "dark");
-        isDark = true; }
+        isDark = true;}
 });
 
 
